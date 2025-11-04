123d60462f5a0f6f734ead92c3d3bf072dc21bbc
icrmx14 <raiujjwalr@gmail.com>
2025-11-05 01:01:27 +0600

    Initial commit of HAProxy project

---
+++ app/server1.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    # Return a simple message identifying this server
+    return '<h1>Hello from Server 1!</h1>'
+
+if __name__ == '__main__':
+    # Run on port 5001
+    app.run(host='0.0.0.0', port=5001)
